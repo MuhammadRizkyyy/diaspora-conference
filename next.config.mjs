2467b05334bd807ea798cedf676a8e63c94bab1f
kiki <rizkyshakira2017@gmail.com>
2026-09-14 06:46:48 +0700

    chore: set up deploymeny statis to github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  basePath: "/diaspora-conference",
+  images: { unoptimized: true },
+  trailingSlash: true,
+};
 
 export default nextConfig;
